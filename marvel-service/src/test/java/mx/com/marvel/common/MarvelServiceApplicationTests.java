cda3111941e976c68ef1f5fa6b1610c9bd91f400
rmiranda <rafmirand@gmail.com>
2023-12-07 16:29:07 -0600

    Estructura de proyecto api consumer y api servicio

---
+++ marvel-service/src/test/java/mx/com/marvel/common/MarvelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.marvel.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
